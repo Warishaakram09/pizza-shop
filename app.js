24951193b4961c2fd4252bd2f8ce77875e0bc1dc
Warisha Akram <160777669+Warishaakram09@users.noreply.github.com>
2024-10-03 17:38:36 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+var orderBtn = document.getElementById('orderBtn');
+orderBtn === null || orderBtn === void 0 ? void 0 : orderBtn.addEventListener('click', function () {
+    alert('Thank you for your order!');
+});
